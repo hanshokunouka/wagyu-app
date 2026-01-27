b52a12e12aa7f2894da6ddc7d316cc9dfa8b2d78
hanshokunouka <newgame02040204@gmail.com>
2026-01-27 17:57:50 +0900

    sw.js の更新

--- sw.js
+++ sw.js
@@ -1,19 +1,15 @@
-const CACHE_NAME = "breeder285-v3";
-
+const CACHE_NAME = "breeder285-v1";
 const urlsToCache = [
   "./",
   "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+  "./manifest.json"
 ];
 
 self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function (cache) {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
@@ -33,9 +29,8 @@ self.addEventListener("activate", function (event) {
 
 self.addEventListener("fetch", function (event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function (response) {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
   );
 });
