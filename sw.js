701a90af39082d16cbd564135a3854d70eca67bc
hanshokunouka <newgame02040204@gmail.com>
2026-01-27 21:30:07 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "breeder285-v7"; // バージョンを上げる
+const CACHE_NAME = "breeder285-v10"; // 数字を大きくして更新を強制
 const urlsToCache = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./icon.png" // アイコンもキャッシュに含める
+  "./icon.png"
 ];
 
 self.addEventListener("install", (event) => {
@@ -13,7 +13,6 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-  // ネットワークから最新を取得し、ダメならキャッシュを出す設定
   event.respondWith(
     fetch(event.request).catch(() => caches.match(event.request))
   );
